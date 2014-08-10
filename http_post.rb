0e09f0844b5f8609b34e79f70ea9afe255bacbc0
ryota kunisada <r.kunisada661@gmail.com>
2014-08-10 16:22:11 +0900

    Add colum User:reg_id

--- http_post.rb
+++ http_post.rb
@@ -1,14 +1,15 @@
 require "net/http"
 require "uri"
 
-uri = URI.parse("http://localhost:3000/users/register")
+uri = URI.parse("http://localhost:3000/user/register")
 Net::HTTP.start(uri.host, uri.port){|http|
   #ヘッダー部
   header = {
     "user-agent" => "Ruby/#{RUBY_VERSION} MyHttpClient"
  }
   #ボディ部
-  body = "registration_id=APA91bGuUlHkxeVT5fcGp3OBUkCy8I2gXo-4SQTrwcnpD3FjCAoYNRrurVS54aFJCAqGYgWCA_gheTESrDj6Bhv6EwGigY3mY5UExUNBv2NM9RicRXBOVJCJFX0wJUd_jJTeBDTaj-MQFJ8Fts27_5BH8WI5na5hGQ"
+  body = "email=testttt@gmail.com&pw=abcdefghijk"
   #送信
   response = http.post(uri.path, body, header)
+  puts response
 }
